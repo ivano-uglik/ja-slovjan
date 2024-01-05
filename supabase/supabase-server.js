ae357218d29e1a1524d5d055f7835fdf7fa51fbf
Teo-Medesi <teo.medesi@gmail.com>
2024-01-05 18:01:30 +0100

    created some basic supabase code

--- supabase/supabase-server.js
+++ supabase/supabase-server.js
@@ -1,8 +1,12 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 
 const supabase = createServerComponentClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  { cookies },
+  {
+    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  }
 );
 
 export default supabase;
