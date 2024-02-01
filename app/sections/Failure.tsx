59f0ee6862807e4dcf6d42af4114258b29a9932c
Teo-Medesi <teo.medesi@gmail.com>
2024-02-01 14:09:48 +0100

    added failure bear png

--- app/sections/Failure.tsx
+++ app/sections/Failure.tsx
@@ -1,7 +1,9 @@
 "use client";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import Bear from "@/public/failure-bear.png";
 import React from "react";
+import Image from "next/image";
 
 const Failure = () => {
   const router = useRouter();
@@ -12,13 +14,23 @@ const Failure = () => {
 
   return (
     <div className="flex w-full h-screen justify-center items-center flex-col typography">
-      <img src="" alt="" />
+      <Image
+        className="w-96 aspect-square"
+        src={Bear}
+        alt="sad bear holding his thumbs down"
+      />
       <h1 className="mb-2 text-4xl font-bold text-base-content">Failure!</h1>
       <p className="mb-8 text-left md:text-center text-zinc-500">
-        An error somewhere ocurred! Please{" "}
-        <Link href="mailto:jaslovjan@gmail.com">contact the admin</Link> or open
-        an issue on{" "}
-        <Link href="https://github.com/ivano-uglik/ja-slovjan">
+        An error ocurred somewhere! Please{" "}
+        <Link
+          className="text-error font-bold hover:underline"
+          href="mailto:jaslovjan@gmail.com">
+          contact the admin
+        </Link>{" "}
+        or open an issue on{" "}
+        <Link
+          className="text-error font-bold hover:underline"
+          href="https://github.com/ivano-uglik/ja-slovjan">
           the github page
         </Link>
       </p>
